58b34d8b10320275d0486ac6e79dc162403a1d3e
9itamar9 <9itamar9@gmail.com>
2023-09-15 08:18:11 +0300

    Reverted the symlink or the profile.ps1 to be in te git

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -1,1 +1,28 @@
-C:/Users/Dell/Documents/WindowsPowerShell/profile.ps1
+# git aliases
+FUNCTION git_status_short {git status -sb $args}
+Set-Alias gst git_status_short
+
+FUNCTION git_push {git push $args}
+New-Alias gp git_push -force -Option AllScope
+
+FUNCTION git_pull {git pull $args}
+Set-Alias gl git_pull -force -Option AllScope
+
+FUNCTION git_diff {git diff $args}
+Set-Alias gd git_diff
+
+FUNCTION git_add {git add $args}
+Set-Alias ga git_add
+
+FUNCTION git_commit_all_message {git commit -am $args}
+Set-Alias gcam git_commit_all_message
+
+# misc
+Set-Alias vim nvim
+
+FUNCTION last_vim {nvim -c 'norm \`0'}
+Set-Alias lvim last_vim
+
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
